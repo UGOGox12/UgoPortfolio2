6eaea7a737962a299cc9cfe5f4ba815c1e170cc4
UGOGox12 <wronggweg@gmail.com>
2025-08-11 23:13:38 +0100

    another s change

--- src/About.jsx
+++ src/About.jsx
@@ -20,7 +20,7 @@ export default function About(){
       <div className="about-content text-center"></div>
       <h3 className="text-4xl ">Frontend Developer !</h3>
       <p>
-        Lorem dolor sit amet consectetur adipisicing elit. Enim vero
+        dolor sit amet consectetur adipisicing elit. Enim vero
         voluptate nisi porro possimus voluptatum tenetur libero repudiandae
         incidunt ipsum necessitatibus facere, nulla adipisci mollitia obcaecati
         qui, exercitationem ullam fugiat. Ex, alias est repellendus dicta
